76dff1203923469d56f980c60704f659a4651940
Aidan <survivalking4@gmail.com>
2023-03-28 22:23:18 -0700

    rsj

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 var assignArray = [];
 var hiding = [];
 var editing = false;
+var results;
 
-async function main() {
+async function downloadAsync() {
+    loader.innerHTML = "Loading...reading cached values<br><br>"
+    results = await getJSON("https://aidanjacobson.duckdns.org:2222/diffeqhw/all");
+    localStorage.setItem("hw_cache", JSON.stringify(results));
+    loader.setAttribute("hidden", true);
+    main(true);
+}
+
+async function main(prevent=false) {
     if (localStorage.getItem("hiding")) {
         hiding = JSON.parse(localStorage.getItem("hiding"));
     }
-    var results = await getJSON("https://aidanjacobson.duckdns.org:2222/diffeqhw/all");
-    loader.setAttribute("hidden", true);
+    if (localStorage.getItem("hw_cache")) {
+        results = JSON.parse(localStorage.getItem("hw_cache"));
+        if (!prevent) downloadAsync();
+    } else {
+        results = await getJSON("https://aidanjacobson.duckdns.org:2222/diffeqhw/all");
+        loader.setAttribute("hidden", true);
+        localStorage.setItem("hw_cache", JSON.stringify(results));
+    }
     assignArray = [];
     for (assign in results) {
         assignArray.push({name:assign,problems:results[assign]});
@@ -26,6 +41,9 @@ async function getJSON(url) {
         x.open("GET", url);
         x.onload = function() {
             resolve(JSON.parse(x.responseText));
+        }
+        x.onerror = function(e) {
+            alert(e.message);
         }
         x.send();
     })
